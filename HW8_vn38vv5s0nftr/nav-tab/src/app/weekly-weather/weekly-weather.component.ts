365d1bb203b056c3316f472dfa8047d45705e2e3
Samuel3Shin <samuel3.shin@gmail.com>
2021-10-26 16:28:29 -0700

    refactored weekly weather with bootstrap table

--- HW8_vn38vv5s0nftr/nav-tab/src/app/weekly-weather/weekly-weather.component.ts
+++ HW8_vn38vv5s0nftr/nav-tab/src/app/weekly-weather/weekly-weather.component.ts
@@ -5,6 +5,15 @@ import { Utils } from './../utils';
 
 var util = new Utils();
 
+interface WeatherDetail {
+  date: string;
+  status_img: string;
+  status_txt: string;
+  high_temp: string;
+  low_temp: string;
+  wind_speed: string;
+}
+
 @Component({
   selector: 'app-weekly-weather',
   templateUrl: './weekly-weather.component.html',
@@ -15,6 +24,8 @@ export class WeeklyWeatherComponent implements OnInit {
   @Output() sendMyEvent : EventEmitter<any> = new EventEmitter();
   @Output() sendDetailTrigger : EventEmitter<any> = new EventEmitter();
 
+  weatherDetails: WeatherDetail[] = [];
+
   constructor() { }
 
   ngOnInit(): void {
@@ -22,7 +33,7 @@ export class WeeklyWeatherComponent implements OnInit {
     // this.sendMyEvent.emit(this.json_data['data']);
   }
 
-  onItemClick(event: any, _item: any){
+  onItemClick(event: any){
     // alert("Event type: " + event.toDateString());
     this.sendDetailTrigger.emit(event);
     // return (event.target as HTMLInputElement).value;
@@ -33,39 +44,25 @@ export class WeeklyWeatherComponent implements OnInit {
     if(this.json_data != null) {
         this.json_data = JSON.parse(this.json_data);
         this.sendMyEvent.emit(this.json_data['data']);
-
-        var weekly_weather_cards = document.getElementsByClassName("weekly_weather_card");
         
         for(var i=0; i<this.json_data["data"]["timelines"][2]["intervals"].length; ++i) {
-            var board = weekly_weather_cards[i];
-
-            // board.style.visibility = "visible";
-    
-            // board.addEventListener("click", showWeeklyWeather(json_data, i));
-            board.getElementsByClassName("idx")[0].innerHTML = (i+1) + "";
-
             var date = new Date(this.json_data["data"]["timelines"][2]["intervals"][i]["startTime"]);
     
-            board.getElementsByClassName("date")[0].innerHTML  = util.weekday[date.getDay()] + ", " + date.getDate() + " " + util.month[date.getMonth()] + " " + date.getFullYear();
-            var clickElement = board.getElementsByClassName("date")[0] as HTMLElement;
-            clickElement.addEventListener("click", this.onItemClick.bind(this, date));
+            // var clickElement = board.getElementsByClassName("date")[0] as HTMLElement;
+            // clickElement.addEventListener("click", this.onItemClick.bind(this, date));
 
             const [weather_icon_img_dir, weather_icon_text] = util.weatherCodeMapper(this.json_data["data"]["timelines"][2]["intervals"][i]["values"]["weatherCode"], false);
             
-            // $("#status_img").attr("src", weather_icon_img_dir);
-            var img_set = board.getElementsByClassName("status_img")[0] as HTMLImageElement;
-            img_set.src = weather_icon_img_dir;
+            this.weatherDetails?.push({
+              date: util.weekday[date.getDay()] + ", " + date.getDate() + " " + util.month[date.getMonth()] + " " + date.getFullYear(),
+              status_img: weather_icon_img_dir,
+              status_txt: weather_icon_text,
+              high_temp: this.json_data["data"]["timelines"][2]["intervals"][i]["values"]["temperatureMax"],
+              low_temp: this.json_data["data"]["timelines"][2]["intervals"][i]["values"]["temperatureMin"],
+              wind_speed: this.json_data["data"]["timelines"][2]["intervals"][i]["values"]["windSpeed"]
+            });
 
-            board.getElementsByClassName("status")[0].innerHTML = weather_icon_text;
-    
-            board.getElementsByClassName("temp_high")[0].innerHTML = this.json_data["data"]["timelines"][2]["intervals"][i]["values"]["temperatureMax"];
-            board.getElementsByClassName("temp_low")[0].innerHTML = this.json_data["data"]["timelines"][2]["intervals"][i]["values"]["temperatureMin"];
-            board.getElementsByClassName("wind_speed")[0].innerHTML = this.json_data["data"]["timelines"][2]["intervals"][i]["values"]["windSpeed"];
-            
         }
-
     }
-    
   }
-
 }
